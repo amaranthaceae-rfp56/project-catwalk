54ce2640baba8ce84615c1bad57a60ae7b72426d
David Han <davidhan2392@gmail.com>
2021-10-30 15:17:39 -0700

    added review context

--- client/src/context/reviews/ReviewState.js
+++ client/src/context/reviews/ReviewState.js
@@ -1,7 +1,7 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import Axios from 'axios';
-import ReviewReducer from './reviewReducer';
-import ReviewContext from './reviewContext';
+import ReviewReducer from './ReviewReducer';
+import ReviewContext from './ReviewContext';
 
 const API_URL = 'http://localhost:3000/api/reviews';
 
@@ -19,8 +19,12 @@ const ReviewState = props => {
     reviewMeta: {}
   }
 
-  const getReviews = () => {
-    const res = Axios.get(`${API_URL}/40344`);
+  useEffect(() => {
+    getReviews();
+  }, []);
+
+  const getReviews = async () => {
+    const res = await Axios.get(`${API_URL}/40344`);
 
     dispatch({
       type: 'GET_REVIEWS',
@@ -31,7 +35,7 @@ const ReviewState = props => {
   const [state, dispatch] = useReducer(ReviewReducer, initialState);
 
   return (
-    <ReviewContext.Provider values={{ reviews: state.reviews, reviewMeta: state.reviewMeta, getReviews }}>
+    <ReviewContext.Provider value={{ reviews: state.reviews, reviewMeta: state.reviewMeta, getReviews }}>
       {props.children}
     </ReviewContext.Provider>
   )
