66e284aafa887123880eb066fd53e2914715921e
David Han <davidhan2392@gmail.com>
2021-10-31 14:54:52 -0700

    added folders for each product overview component

--- client/src/components/ProductOverview/ProductOverview.jsx
+++ client/src/components/ProductOverview/ProductOverview.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import ProductContext from '../../context/products/ProductContext';
 
 const ProductOverview = () => {
+  const productContext = useContext(ProductContext);
+
   return (
     <div data-testid = {'Product-Overview'}>
       PRODUCT OVERVIEW
