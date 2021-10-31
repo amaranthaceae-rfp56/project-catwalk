b1b636c8bddc8a01a68a551841100dd02e3954f4
David Han <davidhan2392@gmail.com>
2021-10-31 15:46:36 -0700

    added product image gallery component to overview

--- client/src/components/ProductOverview/ProductOverview.jsx
+++ client/src/components/ProductOverview/ProductOverview.jsx
@@ -1,16 +1,28 @@
 import React, { useContext } from 'react';
 import ProductContext from '../../context/products/ProductContext';
+
 import ProductStarRating from './product-star-rating/product-star-rating.component.jsx';
+import ProductImageGallery from './product-image-gallery/product-image-gallery.component.jsx';
+
+import './ProductOverview.styles.scss';
 
 const ProductOverview = () => {
   const productContext = useContext(ProductContext);
   const { productInfo: { name, slogan, description, category, default_price, features } } = productContext;
   return (
-    <div data-testid = {'Product-Overview'}>
-      <ProductStarRating />
-      <h4>Category: {category}</h4>
-      <h4>name: {name}</h4>
-      <h4>Price: {default_price}</h4>
+    <div className="product-overview-container">
+
+      <div className="product-overview-container-left">
+        <ProductImageGallery />
+      </div>
+
+      <div className="product-overview-container-right">
+        <ProductStarRating />
+        <h4>Category: {category}</h4>
+        <h4>name: {name}</h4>
+        <h4>Price: {default_price}</h4>
+      </div>
+
     </div>
   );
 };
