6a3969017d4064035a8f3c917fef260990352c0c
Carlos Morales <cmorpv@gmail.com>
2021-11-01 13:51:09 -0700

    Working on adding a questions list that dynamically renders

--- client/src/components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
+++ client/src/components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ProductContext from '../../../context/questions/QuestionContext';
 import '../../styles/sections/_questions.scss';
 
 const QuestionsAndAnswers = () => {
   return (
     <div>
-      <div data-testid = {'Questions-And-Answers'}>
-        QUESTIONS AND ANSWERS
+    <div className="questions-container">
+      <div className="questions-idContainer">
+        <div className="questions-title">Q:</div>
       </div>
-      <div className="questions-title">
-        questions and answers
+      <div className="questions-bodyContainer">
+        <div className="questions-title">This is a question?</div>
       </div>
     </div>
+    <div className="questions-container">
+      <div className="questions-idContainer">
+        <div className="questions-title">A:</div>
+      </div>
+      <div className="questions-bodyContainer">
+        <div className="questions-answer">This is one answer</div>
+        <div className="questions-answer">This is another one of those answers</div>
+        <div className="questions-answer">And so we continue with more answers</div>
+        <div className="questions-answer">No one gets tired, more answers</div>
+      </div>
+    </div>
+    </div>
+
+
+  // <div>
+  //   <div data-testid = {'Questions-And-Answers'}>
+  //     QUESTIONS AND ANSWERS
+  //   </div>
+  //   <div className="questions-title">
+  //     questions and answers
+  //   </div>
+  // </div>
   );
 };
 
