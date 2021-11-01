5ef0c35a8c81cd45563fc2f885d4080e2bcac626
David Han <davidhan2392@gmail.com>
2021-10-31 19:47:32 -0700

    changing images on gallery on click

--- client/src/components/ProductOverview/product-image-gallery/product-image-gallery.component.jsx
+++ client/src/components/ProductOverview/product-image-gallery/product-image-gallery.component.jsx
@@ -1,38 +1,45 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import ProductContext from '../../../context/products/ProductContext';
 
 import './product-image-gallery.styles.scss';
 
 const ProductImageGallery = () => {
-  const { currentStyle } = useContext(ProductContext);
-  const [mainPhoto, setMainPhoto] = useState({});
+  const productContext = useContext(ProductContext);
+  const [mainPhoto, setMainPhoto] = useState(null);
+
+  // useEffect(() => {
+
+  // }, [])
 
   const handleClick = (e) => {
-    console.log(e.target.name);
     const currentIndex = Number(e.target.name);
-
-    const selectedPhoto = currentStyle.photos[currentIndex];
+    const selectedPhoto = productContext.currentStyle.photos[currentIndex];
     setMainPhoto(selectedPhoto);
   }
 
-  return (
-    <div className="product-image-gallery-container">
+    return (
+      <div className="product-image-gallery-container">
 
-      <div className="product-image-gallery-thumbnail-container">
-        {currentStyle.photos && currentStyle.photos.map((photo, index) => (
-          <div>
-            <img src={photo.thumbnail_url} className="image-gallery-thumbnail" onClick={handleClick} name={index}/>
-          </div>
-        ))}
+        <div className="product-image-gallery-thumbnail-container">
+          {productContext.currentStyle.photos && productContext.currentStyle.photos.map((photo, index) => (
+            <div>
+              <img src={photo.thumbnail_url} className="image-gallery-thumbnail" onClick={handleClick} name={index}/>
+            </div>
+          ))}
+        </div>
+
+        <div className="product-image-gallery-main">
+            {!mainPhoto ? productContext.currentStyle.photos && productContext.currentStyle.photos.map((photo, i) => {
+              if (i === 0) {
+                return (
+                  <img className="image-gallery-main" src={photo.url} />
+                )
+              }
+            }) : <img className="image-gallery-main" src={mainPhoto.url} /> }
+        </div>
+
       </div>
-
-      <div className="product-image-gallery-main">
-          {!mainPhoto && currentStyles.photos ? <img src={currentStyle.photos[0].url} /> : <img src={mainPhoto.url} />}
-      </div>
-
-      <p>{currentStyle.name}</p>
-    </div>
-  )
+    );
 }
 
 export default ProductImageGallery
