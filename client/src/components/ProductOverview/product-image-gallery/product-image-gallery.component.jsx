bf9abe070b4f4bc8249c75ca0e14ff327dcbc56b
David Han <davidhan2392@gmail.com>
2021-10-31 18:38:27 -0700

    product image updating on click

--- client/src/components/ProductOverview/product-image-gallery/product-image-gallery.component.jsx
+++ client/src/components/ProductOverview/product-image-gallery/product-image-gallery.component.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import ProductContext from '../../../context/products/ProductContext';
+
 import './product-image-gallery.styles.scss';
 
 const ProductImageGallery = () => {
+  const { currentStyle } = useContext(ProductContext);
+  const [mainPhoto, setMainPhoto] = useState({});
+
+  const handleClick = (e) => {
+    console.log(e.target.name);
+    const currentIndex = Number(e.target.name);
+
+    const selectedPhoto = currentStyle.photos[currentIndex];
+    setMainPhoto(selectedPhoto);
+  }
+
   return (
     <div className="product-image-gallery-container">
-      <h4>THIS IS THE PRODUCT IMAGE GALLERY</h4>
+
+      <div className="product-image-gallery-thumbnail-container">
+        {currentStyle.photos && currentStyle.photos.map((photo, index) => (
+          <div>
+            <img src={photo.thumbnail_url} className="image-gallery-thumbnail" onClick={handleClick} name={index}/>
+          </div>
+        ))}
+      </div>
+
+      <div className="product-image-gallery-main">
+          {!mainPhoto && currentStyles.photos ? <img src={currentStyle.photos[0].url} /> : <img src={mainPhoto.url} />}
+      </div>
+
+      <p>{currentStyle.name}</p>
     </div>
   )
 }
