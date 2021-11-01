d5d2a00b73b7fe4ac6e6b67ae58d49aef7747d60
David Han <davidhan2392@gmail.com>
2021-11-01 15:47:05 -0700

    added functionality to only show 15 if there are more than 15 in stock

--- client/src/components/ProductOverview/product-dropdown/product-dropdown.component.jsx
+++ client/src/components/ProductOverview/product-dropdown/product-dropdown.component.jsx
@@ -20,19 +20,23 @@ const ProductDropdown = () => {
   }
 
   return (
-    <div>
-      <select onChange={handleChange} value={quantitySize}>
-        <option value="" selected disabled hidden>Select Size</option>
+    <div className="product-dropdown-container">
+      <select onChange={handleChange} value={quantitySize} className="product-dropdown">
+        <option value="" className="product-dropdown-option" selected disabled hidden >Select Size</option>
         {currentStyle.skus && Object.values(currentStyle.skus).map((sku) => (
           <option value={sku.quantity}>{sku.size}</option>
         ))}
       </select>
 
-      <select onChange={handleQuantityChange} value={quantity}>
+      <select onChange={handleQuantityChange} value={quantity} className="product-dropdown">
       {/* <option value="" selected disabled hidden> - </option> */}
-         {[...Array(quantitySize)].map((num, i) => (
-          <option value={i + 1}>{i + 1}</option>
-        ))}
+         {[...Array(quantitySize)].map((num, i) => {
+           if (i + 1 > 15) {
+             return
+           } else {
+             return (<option value={i + 1}>{i + 1}</option>);
+           }
+         })}
       </select>
       {/* <select>
         <option value="" selected disabled hidden>Select Size</option>
