ae345b8ae3509fd3c9c9bfb21f0fe80309e2012d
David Han <davidhan2392@gmail.com>
2021-11-01 16:09:32 -0700

    updated styling on star ratings

--- client/src/components/ProductOverview/ProductOverview.jsx
+++ client/src/components/ProductOverview/ProductOverview.jsx
@@ -23,10 +23,10 @@ const ProductOverview = () => {
       <div className="product-overview-container-right">
         <ProductStarRating />
         <div className="product-overview-details">
-          <h4>{currentStyle.name}</h4>
-          {!currentStyle.sale_price ? <h5>{currentStyle.original_price}</h5> : <div> <strike style={{ color: "red"}}>{currentStyle.original_price}</strike><h6>{currentStyle.sale_price}</h6></div>}
+          <h2>{currentStyle.name}</h2>
+          {!currentStyle.sale_price ? <h3>$ {currentStyle.original_price}</h3> : <div> <strike style={{ color: "red"}}>$ {currentStyle.original_price}</strike><h3>$ {currentStyle.sale_price}</h3></div>}
         </div>
-        <h6> Style: Selected Style</h6>
+        <h4> Style: Selected Style</h4>
         <ProductStyleSelector />
         <ProductDropdown />
       </div>
