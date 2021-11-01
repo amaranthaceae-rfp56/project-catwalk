980a69537ddb08866777392526914b5d85fb8eb1
David Han <davidhan2392@gmail.com>
2021-11-01 15:36:59 -0700

    created dropdown and quantity dropdown

--- client/src/components/ProductOverview/product-dropdown/product-dropdown.component.jsx
+++ client/src/components/ProductOverview/product-dropdown/product-dropdown.component.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import ProductContext from '../../../context/products/ProductContext';
 
 import './product-dropdown.styles.scss';
@@ -6,16 +6,40 @@ import './product-dropdown.styles.scss';
 const ProductDropdown = () => {
   const productContext = useContext(ProductContext);
   const { currentStyle } = productContext;
+  const [quantitySize, setQuantitySize] = useState(null);
+  const [quantity, setQuantity] = useState(null);
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    setQuantitySize(Number(e.target.value));
+  }
+
+  const handleQuantityChange = (e) => {
+    console.log(e.target.value);
+    setQuantity(e.target.value);
+  }
 
   return (
     <div>
-      <select>
+      <select onChange={handleChange} value={quantitySize}>
         <option value="" selected disabled hidden>Select Size</option>
         {currentStyle.skus && Object.values(currentStyle.skus).map((sku) => (
-          <option>{sku.size}</option>
+          <option value={sku.quantity}>{sku.size}</option>
         ))}
-        {/* <option>test</option> */}
       </select>
+
+      <select onChange={handleQuantityChange} value={quantity}>
+      {/* <option value="" selected disabled hidden> - </option> */}
+         {[...Array(quantitySize)].map((num, i) => (
+          <option value={i + 1}>{i + 1}</option>
+        ))}
+      </select>
+      {/* <select>
+        <option value="" selected disabled hidden>Select Size</option>
+        {currentStyle.skus && Object.values(currentStyle.skus).map((sku) => (
+          <option>{sku.quantity}</option>
+        ))}
+      </select> */}
     </div>
   )
 }
