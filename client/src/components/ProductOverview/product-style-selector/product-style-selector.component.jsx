c3be80d2b368d18a7ac5a930fd7363175afa68c9
David Han <davidhan2392@gmail.com>
2021-11-02 12:11:55 -0700

    checkmark defaults to first item

--- client/src/components/ProductOverview/product-style-selector/product-style-selector.component.jsx
+++ client/src/components/ProductOverview/product-style-selector/product-style-selector.component.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect, Fragment } from 'react';
 import ProductContext from '../../../context/products/ProductContext';
 
 import CheckMark from '../../../../assets/checkmark.svg';
@@ -8,8 +8,6 @@ import './product-style-selector.styles.scss';
 const ProductStyleSelector = () => {
   const { productStyles : { results }, getCurrentStyle } = useContext(ProductContext);
   const [selected, setSelected] = useState({});
-  // const { results } = productContext.productStyles
-  // console.log(results);
 
   const handleClick = (e) => {
     getCurrentStyle(e.target.name);
@@ -18,11 +16,19 @@ const ProductStyleSelector = () => {
 
   return (
     <div className="product-style-container">
-      {results && results.map((style) => (
-        <div className="product-style-icon-container">
-          <img src={CheckMark} className={selected.id == style.style_id ? "checkmark active" : "checkmark"}/>
-          {/* <img src={CheckMark} className={selected.id == style.style.id ? "checkmark active" : "checkmark"} onClick={handleClick}/> */}
-          <img className={selected.id == style.style_id ? "product-style-icon active" : "product-style-icon"} src={style.photos[0].thumbnail_url} name={style.style_id} onClick={handleClick}/>
+      {results && results.map((style, i, key) => (
+        <div className="product-style-icon-container" key={style.style_id}>
+          {!selected.id && i === 0 ?
+          <>
+          <img src={CheckMark} className="checkmark active" />
+          <img className="product-style-icon" src={style.photos[0].thumbnail_url} name={style.style_id} onClick={handleClick}/>
+          </>
+          :
+          <>
+            <img src={CheckMark} className={selected.id == style.style_id ? "checkmark active" : "checkmark"}/>
+            <img className="product-style-icon" src={style.photos[0].thumbnail_url} name={style.style_id} onClick={handleClick}/>
+          </>
+          }
         </div>
       ))}
     </div>
