39e03d606fc96ff84ac9f8380a534625af22506f
Carlos Morales <cmorpv@gmail.com>
2021-11-02 14:16:26 -0700

    Added basic version of load more answers

--- client/src/components/QuestionsAndAnswers/AnswerList.jsx
+++ client/src/components/QuestionsAndAnswers/AnswerList.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../../styles/sections/_questions.scss';
 
 const AnswerList = (props) => {
 
   let questionAnswers = Object.values(props.questionAnswers);
 
+  const [visibleAnswers, setVisibleAnswers] = useState(2);
+  let moreAnswers;
+
+
+  const loadMoreAnswers = () => {
+    if (visibleAnswers + 2 <= questionAnswers.length) {
+      setVisibleAnswers(visibleAnswers + 2);
+    } else {
+      setVisibleAnswers(questionAnswers.length);
+    }
+  };
+
+  if (questionAnswers.length > visibleAnswers) {
+    moreAnswers = (<div className="load-answers" onClick = {loadMoreAnswers} >load more answers</div>);
+  }
+
   return (
+    <div>
     <div className="questions-answer">
-      {questionAnswers.map((answer) => (
+      {questionAnswers.filter((answer, index) => index < visibleAnswers)
+        .map((answer) => (
         < AnswerItem key={answer.id} answer={answer.body} />
       ))}
+    </div>
+      <div>{moreAnswers}</div>
     </div>
   )
 };
