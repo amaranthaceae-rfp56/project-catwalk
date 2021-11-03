8bbd6e284e034e408f836f16983bead4de5bb0fa
Carlos Morales <cmorpv@gmail.com>
2021-11-02 21:34:53 -0700

    Implemented mock versions of helful links, report link, and add answer link

--- client/src/components/QuestionsAndAnswers/AnswerItem.jsx
+++ client/src/components/QuestionsAndAnswers/AnswerItem.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import '../../styles/sections/_questions.scss';
 
 const AnswerItem = (props) => {
 
-  console.log(props);
+  const [helpfulCount, setHelpfulCount] = useState(props.answer.helpfulness);
+
+  const addHelpfulCount = () => {
+    setHelpfulCount(helpfulCount + 1);
+  };
+
+  let foundHelpful = (<div className="found-helpful-font" onClick={addHelpfulCount} >Yes</div>);
+
+  let doReport = (<div className="found-helpful-font" onClick={() => {alert('This answer was offensive')}} >Report</div>);
 
   let dateAnswered = new Date(props.answer.date).toLocaleDateString(undefined,{month: 'long', day: 'numeric', year: 'numeric'});
+
+
 
   return (
     <div>
@@ -16,8 +26,14 @@ const AnswerItem = (props) => {
     <div className="questions-container answers-info-font">
       <div className="answer-footnote-container">{`by ${props.answer.answerer_name}`}</div>
       <div className="answer-footnote-container">{dateAnswered}</div>
-      <div className="answer-footnote-container">{`Helpful? Yes (${props.answer.helpfulness})`}</div>
-      <div className="answer-footnote-container">Report</div>
+      <div className="answer-footnote-container">
+        <div>{`Helpful?  `}</div>
+        <div>{foundHelpful}</div>
+        {`  (${helpfulCount})`}
+      </div>
+      <div className="answer-footnote-container">
+        <div>{doReport}</div>
+      </div>
     </div>
     </div>
   )
