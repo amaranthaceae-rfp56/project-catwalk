170bb6e2f7861e90efad2a7d6c525523203ad838
Christopher Schillinger <cschillinger1994@gmail.com>
2021-11-04 14:23:06 -0500

    Created close functionality for modal component

--- client/src/components/RatingsAndReviews/Reviews/PhotoTiles.jsx
+++ client/src/components/RatingsAndReviews/Reviews/PhotoTiles.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Modal from '../../sharedComponents/Modal.jsx';
+import PhotoModal from './PhotoModal.jsx';
 
 
-const photos = [
-  'https://source.unsplash.com/random',
-  'https://source.unsplash.com/random',
-  'https://source.unsplash.com/random',
-  'https://source.unsplash.com/random',
-  'https://source.unsplash.com/random',
-  'https://source.unsplash.com/random'
-]
+
 
 const PhotoTiles = (props) => {
+
+  const [modal, setModal] = useState(false);
+
+
   return (
     <div className = 'photos-container'>
       {props.photos.map( (photo, index) => {
-        return (<img className = 'review-photo' key = {`photo-${index}`}src = {photo.url}></img>)
-      })}
+        const clickTile = () => {
+          const selectedPhoto = (<PhotoModal image = {photo.url}/>);
+          setModal(<Modal callback = {setModal} component={selectedPhoto}/>)
+        };
 
+        return (<img onClick = {clickTile} className = 'review-photo' key = {`photo-${index}`}src = {photo.url}></img>)
+      })}
+      {modal}
     </div>
   );
 };
