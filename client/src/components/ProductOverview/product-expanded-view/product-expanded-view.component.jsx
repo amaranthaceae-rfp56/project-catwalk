0d810124cc8caca0fd8dc86e060a5ae33bb394cb
David Han <davidhan2392@gmail.com>
2021-11-04 13:24:28 -0700

    zoom click working

--- client/src/components/ProductOverview/product-expanded-view/product-expanded-view.component.jsx
+++ client/src/components/ProductOverview/product-expanded-view/product-expanded-view.component.jsx
@@ -6,6 +6,8 @@ import rightArrow from '../../../../assets/forwardArrow.svg';
 import upArrow from '../../../../assets/upArrow.svg';
 import downArrow from '../../../../assets/downArrow.svg';
 
+// import ProductMagnify from '../product-magnify.component.jsx';
+
 import './product-expanded-view.styles.scss';
 
 const ProductExpandedView = ({ expandView }) => {
@@ -13,6 +15,8 @@ const ProductExpandedView = ({ expandView }) => {
   const { currentStyle } = productContext;
   const [page, setPage] = useState(0);
   const [height, setHeight] = useState(0);
+  const [zoom, setZoom] = useState();
+  const [zoomState, setZoomState] = useState(false);
   const ref = useRef(null);
 
 
@@ -48,14 +52,52 @@ const ProductExpandedView = ({ expandView }) => {
     ref.current.scrollTop -= height
   }
 
+  const handleZoom = (e, photoUrl) => {
+    setZoomState(!zoomState);
+    const { left, top, width, height } = e.target.getBoundingClientRect();
+    console.log(left, top, width, height);
+    const x = (e.pageX - left) / width * 250;
+    const y = (e.pageY - top) / height * 250;
+    console.log(x, y);
+
+    setZoom({
+      backgroundImage: `url(${photoUrl})`,
+      backgroundPosition: `${x}% ${y}%`,
+    })
+  }
+
+  // const handleZoomScroll = (e) => {
+  //   const { left, top, width, height } = e.target.getBoundingClientRect();
+  //   console.log(left, top, width, height);
+  //   const x = (e.pageX - left) / width * 250;
+  //   const y = (e.pageY - top) / height * 250;
+
+  //   setZoom({
+  //     backgroundPosition: `${x}% ${y}%`,
+  //   })
+  // }
+
     if (!currentStyle.photos) {
       return <p>Loading...</p>
     } else {
     return (
       <div className="expanded-view-container">
 
+          <div className="expanded-view-gallery-main">
+                    <img src={leftArrow} className="expanded-view-left-arrow" onClick={handlePageChange} name="back" value={page}/>
+                    {currentStyle.photos.map((photo, index) => {
+                      if (page === index) {
+                        return (
+                          <figure onClick={(e) => handleZoom(e, photo.url)} style={!zoom ? { backgroundImage: `url(${photo.url})`, backgroundPosition: '0% 0%'} : zoom}>
+                            <img className={!zoomState ? "expanded-view-main" : "expanded-view-main active"} src={photo.url} />
+                          </figure>
+                        )
+                      }
+                    })}
+                    <img src={rightArrow} className="expanded-view-right-arrow" onClick={handlePageChange} name="front" value={page}/>
+          </div>
 
-        <div className="expanded-view-gallery-main">
+        {/* <div className="expanded-view-gallery-main">
                     <img src={leftArrow} className="expanded-view-left-arrow" onClick={handlePageChange} name="back" value={page}/>
                     {currentStyle.photos.map((photo, index) => {
                       if (page === index) {
@@ -65,7 +107,7 @@ const ProductExpandedView = ({ expandView }) => {
                       }
                     })}
                     <img src={rightArrow} className="expanded-view-right-arrow" onClick={handlePageChange} name="front" value={page}/>
-          </div>
+          </div> */}
 
         <div className="expanded-view-thumbnail-container" >
             {currentStyle.photos && currentStyle.photos.map((photo, index, key) => (
---
+++ client/src/components/ProductOverview/product-magnify/product-magnify.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductMagnify = () => {
+
+
+  return (
+    <div>Testing</div>
+  )
+}
+
+export default ProductMagnify
