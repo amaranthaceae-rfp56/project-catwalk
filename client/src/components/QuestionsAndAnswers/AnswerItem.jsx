32df73040743f15a6b70a0418ff92aa019ca2f73
Carlos Morales <cmorpv@gmail.com>
2021-11-05 11:32:45 -0700

    Implemented the helpful answer functionality to update api server helpfulness count

--- client/src/components/QuestionsAndAnswers/AnswerItem.jsx
+++ client/src/components/QuestionsAndAnswers/AnswerItem.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import '../../styles/sections/_questions.scss';
 
 const AnswerItem = (props) => {
@@ -7,8 +8,25 @@ const AnswerItem = (props) => {
   const [helpfulCount, setHelpfulCount] = useState(props.answer.helpfulness);
 
   const addHelpfulCount = () => {
-    setHelpfulCount(helpfulCount + 1);
+
+    const options = {
+      url: `http://localhost:3000/api/qa/answers/:answer_id/helpful/`,
+      method: 'PUT',
+      params: {
+        answer_id: props.answer.id
+      }
+    };
+
+    axios(options)
+      .then(() => {
+        console.log('user found answer helpful!', props.answer.id);
+        setHelpfulCount(helpfulCount + 1);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
+
 
   let foundHelpful = (<div className="found-helpful-font" onClick={addHelpfulCount} >Yes</div>);
 
