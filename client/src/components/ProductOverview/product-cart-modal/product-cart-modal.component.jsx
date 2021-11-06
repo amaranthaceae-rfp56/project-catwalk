aff56dfc55941c94b229fe4fb7a3efdcc7bf058b
David Han <davidhan2392@gmail.com>
2021-11-06 10:00:03 -0700

    added cart modal

---
+++ client/src/components/ProductOverview/product-cart-modal/product-cart-modal.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './product-cart-modal.styles.scss'
+
+const ProductCartModal = () => {
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h4 className="modal-title">Cart</h4>
+        </div>
+
+        <div className="modal-body">
+
+        </div>
+
+        <div className="modal-footer">
+          <button className="modal-button">Close</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCartModal
