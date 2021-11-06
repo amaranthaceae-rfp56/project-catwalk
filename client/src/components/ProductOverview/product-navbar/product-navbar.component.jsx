c95165da5cec60784bd7e9c1461090d3fd5b659c
David Han <davidhan2392@gmail.com>
2021-11-05 18:54:17 -0700

    updated navbar style

--- client/src/components/ProductOverview/product-navbar/product-navbar.component.jsx
+++ client/src/components/ProductOverview/product-navbar/product-navbar.component.jsx
@@ -18,7 +18,7 @@ const ProductNavbar = () => {
           <a href="#related-card-container" style={{ textDecoration: 'none'}}>Related</a>
           <a href="#questions-section-container" style={{ textDecoration: 'none'}}>Questions</a>
           <a href="#ratings-reviews-container" style={{ textDecoration: 'none'}}>Reviews</a>
-          <img src={CartIcon} style={{ height: '50px', width: '50px' }}/>
+          <img src={CartIcon} style={{ height: '40px', width: '40px' }}/>
       </div>
 
     </nav>
