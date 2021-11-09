85d154a8f9f68ecc0c8a923f628d8a73ed1c6558
David Han <davidhan2392@gmail.com>
2021-11-09 10:02:26 -0800

    added product overview components to testing file

--- client/src/spec/ProductOverview/ProductOverview.test.js
+++ client/src/spec/ProductOverview/ProductOverview.test.js
@@ -5,6 +5,10 @@ import React from 'react';
 
 import ProductOverview from '../../components/ProductOverview/ProductOverview.jsx';
 import ProductNavbar from '../../components/ProductOverview/product-navbar/product-navbar.component.jsx'
+import ProductDropdown from '../../components/ProductOverview/product-dropdown/product-dropdown.component.jsx'
+import ProductExpandedView from '../../components/ProductOverview/product-expanded-view/product-expanded-view.component.jsx'
+import ProductImageGallery from '../../components/ProductOverview/product-image-gallery/product-image-gallery.component.jsx'
+import ProductStyleSelector from '../../components/ProductOverview/product-style-selector/product-style-selector.component.jsx'
 
 afterEach(()=> {
   cleanup();
@@ -21,7 +25,7 @@ describe('Product Overview Testing', () => {
   });
 })
 
-describe('Product Navbar Testing', () => {
+describe.only('Product Navbar Testing', () => {
   test("Renders navbar links", () => {
     render(<ProductNavbar />);
 
