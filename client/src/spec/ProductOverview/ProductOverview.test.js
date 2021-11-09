d7af52cf136db3caf78029aaee485174620ddbd2
David Han <davidhan2392@gmail.com>
2021-11-09 09:54:17 -0800

    updated testing to be in productoverview file

--- client/src/spec/ProductOverview/ProductOverview.test.js
+++ client/src/spec/ProductOverview/ProductOverview.test.js
@@ -4,18 +4,32 @@ import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
 
 import ProductOverview from '../../components/ProductOverview/ProductOverview.jsx';
+import ProductNavbar from '../../components/ProductOverview/product-navbar/product-navbar.component.jsx'
 
-afterEach( ()=> {
+afterEach(()=> {
   cleanup();
 });
 
-test('test', () => {
-  render(<ProductOverview/>);
-  const item = screen.getByTestId('Product-Overview');
 
-  expect(item).toBeInTheDocument();
-  expect(item).toHaveTextContent('PRODUCT OVERVIEW');
-});
+describe('Product Overview Testing', () => {
+  test('test', () => {
+    render(<ProductOverview/>);
+    const item = screen.getByTestId('Product-Overview');
+
+    expect(item).toBeInTheDocument();
+    expect(item).toHaveTextContent('PRODUCT OVERVIEW');
+  });
+})
+
+describe('Product Navbar Testing', () => {
+  test("Renders navbar links", () => {
+    render(<ProductNavbar />);
+
+    const navbarLink = screen.getByText("Related");
+    expect(navbarLink).toBeInTheDocument();
+  })
+})
+
 // test('snapshots', () => {
 //   const tree = renderer.create(<ProductOverview/>).toJSON();
 //   console.log(tree);
